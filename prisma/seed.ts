6ceb7da8ff7966dfe487c0c15696adb5ed015404
peno <60010482@kmitl.ac.th>
2025-06-26 21:09:12 +0700

    feat: setup prisma and db

---
+++ prisma/seed.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@/generated/prisma";
+
+const prisma = new PrismaClient();
+async function main() {
+  const admin = await prisma.user.upsert({
+    where: { email: "admin@admin.com" },
+    update: {},
+    create: {
+      email: "admin@admin.com",
+      name: "Admin",
+    },
+  });
+  const bob = await prisma.user.upsert({
+    where: { email: "bob@prisma.io" },
+    update: {},
+    create: {
+      email: "bob@prisma.io",
+      name: "Bob",
+    },
+  });
+  console.log({ admin, bob });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { prisma } from "@/lib/db";
 
-const Page = () => {
+const Page = async () => {
+  const users = await prisma.user.findMany();
+
   return (
     <div className='text-red-500 font-bold'>
-      Hello d<Button>Click me</Button>
+      {users.map((user) => user.name)}
     </div>
   );
 };
