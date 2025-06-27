cf344c7bd17376e57281df62605f8d9905c9bbd9
peno <60010482@kmitl.ac.th>
2025-06-28 00:54:40 +0700

    feat: ai jobs
    chore: git ignore
    
    chore: revert inngest db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPCMutation } from "@/hooks/use-query";
+import { useState } from "react";
 import { toast } from "sonner";
 
 const Page = () => {
+  const [value, setValue] = useState("");
+
   const invoke = useTRPCMutation((trpc) =>
     trpc.invoke.mutationOptions({
       onSuccess: () => toast.success("Invoked successfully"),
@@ -13,10 +17,12 @@ const Page = () => {
   );
 
   return (
-    <div className='text-red-500 font-bold'>
+    <div className='p-6 flex flex-col gap-2 align-start'>
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
       <Button
+        className='w-fit'
         disabled={invoke.isPending}
-        onClick={() => invoke.mutate({ text: "John" })}
+        onClick={() => invoke.mutate({ value })}
       >
         Invoke
       </Button>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,18 +1,21 @@
 import { inngest } from "./client";
+import { gemini, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    // Imagine this is a download video
-    await step.sleep("wait-a-moment", "30s");
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system:
+        "You are an expert next.js developer. You write readable, maintainable code. You write simple Next.js & React snippets.",
+      model: gemini({ model: "gemini-2.0-flash" }),
+    });
 
-    // Imagine this is a transcript step
-    await step.sleep("wait-a-moment", "10s");
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`
+    );
 
-    // Imagine this is a summary step
-    await step.sleep("wait-a-moment", "5s");
-
-    return { message: `Hello ${event.data.email}!` };
+    return { output };
   }
 );
